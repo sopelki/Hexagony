383b51d9a41f668a460cf60f769f52153be3a070
Ярослав Захаров <aroslavzaharov89@gmail.com>
2026-05-25 19:44:32 +0500

    handler fix

--- Assets/Scripts/Misc/UIBlocker.cs
+++ Assets/Scripts/Misc/UIBlocker.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UI;
 
 namespace Misc
@@ -13,6 +14,15 @@ namespace Misc
 
             var allComponents = Object.FindObjectsByType<MonoBehaviour>();
 
+            var currentMousePosition = Vector2.zero;
+            if (Mouse.current != null)
+                currentMousePosition = Mouse.current.position.ReadValue();
+
+            var pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = currentMousePosition
+            };
+
             foreach (var mb in allComponents)
             {
                 switch (mb)
@@ -24,7 +34,12 @@ namespace Misc
 
                     case IBeginDragHandler or IDragHandler or IEndDragHandler:
                         if (mb is not UnityEngine.UI.Slider)
+                        {
+                            if (mb is IEndDragHandler endDragHandler)
+                                endDragHandler.OnEndDrag(pointerData);
+
                             mb.enabled = false;
+                        }
                         break;
                 }
             }
@@ -36,7 +51,8 @@ namespace Misc
 
             foreach (var mb in allComponents)
             {
-                if (mb is TooltipTrigger or IBeginDragHandler or IDragHandler or IEndDragHandler)
+                if (mb is (TooltipTrigger or IBeginDragHandler or IDragHandler or IEndDragHandler)
+                    and not UnityEngine.UI.Slider)
                     mb.enabled = true;
             }
         }
